5259254657c0b780429e9168c048d726675e73f2
Leonardo Menezes <leonardorsm07@gmail.com>
2024-10-10 21:21:24 -0300

    Ajeitado inteligendia do jogador campeão

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -5,57 +5,55 @@ class JogoDaVelha:
         self.modo_jogo = modo_jogo
         self.num_jogos = num_jogos
         self.resultados = {"X": 0, "O": 0, "Empate": 0}
+        self.tabuleiros = []
         self.iniciar_jogos()
 
     def iniciar_jogos(self):
         for _ in range(self.num_jogos):
-            self.tabuleiro = [0] + [0] * 9
+            self.tabuleiro = [0] + [0] * 10
             self.jogador_atual = 1 if random.randint(1, 6) % 2 == 0 else -1
-            print(f"O jogador {'X' if self.jogador_atual == 1 else 'O'} começa!")
+            print(f"\n\nO jogador {'X' if self.jogador_atual == 1 else 'O'} começa!")
             self.jogar()
 
-        if self.num_jogos > 1:  # Exibir resultados se mais de um jogo for jogado
+        if self.num_jogos > 1:
             print("\nResultados:")
             print(f"Vitórias de X: {self.resultados['X']}")
             print(f"Vitórias de O: {self.resultados['O']}")
             print(f"Empates: {self.resultados['Empate']}")
+            print(f"Tabuleiros de todos os jogos: {self.tabuleiros}")
 
     def jogar(self):
         while True:
-            self.mostrar_tabuleiro()  # Mostrar o tabuleiro no início de cada turno
-            if self.modo_jogo in ["JxJ"]:
-                posicao = int(input(f"Jogador {'X' if self.jogador_atual == 1 else 'O'} Escolha uma posição de 1 a 9 para jogar (1 é canto superior esquerdo, 9 é canto inferior direito): ")) - 1
-                if self.tabuleiro[posicao + 1] == 0:
+            if self.modo_jogo == "JxJ":
+                print(self.tabuleiro)
+                self.mostrar_tabuleiro()
+                posicao = int(input(f"Jogador {'X' if self.jogador_atual == 1 else 'O'}, escolha uma posição de 1 a 9 para jogar (1 é canto superior esquerdo, 9 é canto inferior direito): ")) - 1
+                if 0 <= posicao <= 8 and self.tabuleiro[posicao + 1] == 0:
                     self.fazer_jogada(posicao, self.jogador_atual)
-                    
-            if self.modo_jogo in ["JxAleatório", "JxCampeão"]:
-                if self.jogador_atual == 1:  # Jogador humano
-                    posicao = int(input("Escolha uma posição de 1 a 9 para jogar (1 é canto superior esquerdo, 9 é canto inferior direito): ")) - 1
-                    if self.tabuleiro[posicao + 1] == 0:
-                        self.fazer_jogada(posicao, 1)
-                    else:
-                        print("Posição inválida! Tente novamente.")
-                        continue
                 else:
-                    self.jogada_computador()
+                    print("Posição inválida! Tente novamente.")
+                    continue  
             else:
+                self.mostrar_tabuleiro()
                 self.jogada_computador()
-            print(self.tabuleiro)
 
             if self.checar_vencedor():
-                self.mostrar_tabuleiro()  # Mostrar o tabuleiro após a vitória
+                if(self.jogador_atual == 1): self.mostrar_tabuleiro()
                 vencedor = 'X' if self.jogador_atual == 1 else 'O'
                 print(f"O jogador {vencedor} venceu!")
+                self.tabuleiro[10] = vencedor
                 self.resultados[vencedor] += 1
                 break
             elif self.tabuleiro[0] == 9:  # Verifica se deu empate
-                self.mostrar_tabuleiro()
                 print("Empate!")
+                self.tabuleiro[10] = 'V'
                 self.resultados["Empate"] += 1
                 break
 
             # Alterna o jogador
             self.jogador_atual *= -1
+
+        self.tabuleiros.append(self.tabuleiro.copy())
 
     def fazer_jogada(self, posicao, jogador):
         self.tabuleiro[posicao + 1] = jogador
@@ -65,9 +63,16 @@ class JogoDaVelha:
         if self.modo_jogo in ["JxAleatório", "Aleatório_vs_Aleatório"]:
             print("Jogada do computador aleatório...")
             self.jogada_aleatoria()
-        elif self.modo_jogo in ["JxCampeão", "Aleatório_vs_Campeão", "Campeão_vs_Campeão"]:
+        elif self.modo_jogo in ["JxCampeão", "Campeão_vs_Campeão"]:
             print("Jogada do computador campeão...")
             self.jogada_campeao()
+        elif self.modo_jogo == "Aleatório_vs_Campeão":
+            if self.jogador_atual == 1:
+                print("Jogada do computador aleatório...")
+                self.jogada_aleatoria()
+            else:
+                print("Jogada do computador campeão...")
+                self.jogada_campeao()
 
     def jogada_aleatoria(self):
         while True:
@@ -79,31 +84,65 @@ class JogoDaVelha:
     def jogada_campeao(self):
         jogador_adversario = 1 if self.jogador_atual == -1 else -1
 
-        # Se for centro, joga nele
-        if self.tabuleiro[5] == 0:
-            self.fazer_jogada(4, self.jogador_atual)
-            return
-
-        # Bloqueia o outro jogador
-        for posicao in range(9):
-            if self.tabuleiro[posicao + 1] == 0:
-                self.tabuleiro[posicao + 1] = jogador_adversario
-                if self.checar_vencedor():
-                    self.tabuleiro[posicao + 1] = self.jogador_atual  # Aqui é onde a jogada do campeão é feita
-                    self.fazer_jogada(posicao, self.jogador_atual)  # Atualiza o vetor de jogadas
-                    return
-                self.tabuleiro[posicao + 1] = 0
-
-        # Tenta vencer
         for posicao in range(9):
             if self.tabuleiro[posicao + 1] == 0:
                 self.tabuleiro[posicao + 1] = self.jogador_atual
                 if self.checar_vencedor():
-                    self.fazer_jogada(posicao, self.jogador_atual)  # Atualiza o vetor de jogadas
+                    self.fazer_jogada(posicao, self.jogador_atual)
                     return
                 self.tabuleiro[posicao + 1] = 0
 
-        self.jogada_aleatoria()  # Se nenhuma jogada estratégica for feita, joga aleatoriamente
+        for posicao in range(9):
+            if self.tabuleiro[posicao + 1] == 0:
+                self.tabuleiro[posicao + 1] = jogador_adversario
+                if self.checar_vencedor():
+                    self.tabuleiro[posicao + 1] = self.jogador_atual
+                    self.fazer_jogada(posicao, self.jogador_atual)
+                    return
+                self.tabuleiro[posicao + 1] = 0
+
+        if self.tabuleiro[5] == 0:
+            self.fazer_jogada(4, self.jogador_atual)
+            return
+
+        if self.tabuleiro[5] == jogador_adversario:
+            if self.tabuleiro[1] == 0:
+                self.fazer_jogada(0, self.jogador_atual)
+                return
+            if self.tabuleiro[3] == 0:
+                self.fazer_jogada(2, self.jogador_atual)
+                return
+            if self.tabuleiro[7] == 0:
+                self.fazer_jogada(6, self.jogador_atual)
+                return
+            if self.tabuleiro[9] == 0:
+                self.fazer_jogada(8, self.jogador_atual)
+                return
+
+        cantos_opostos = [(1, 9), (3, 7)]
+        if not any(self.tabuleiro[pos] != 0 for pos in [2, 4, 6, 8]):
+            for canto1, canto2 in cantos_opostos:
+                if (self.tabuleiro[canto1] == jogador_adversario and
+                    self.tabuleiro[canto2] == jogador_adversario):
+                    for lateral in [2, 4, 6, 8]:
+                        if self.tabuleiro[lateral] == 0:
+                            self.fazer_jogada(lateral - 1, self.jogador_atual)
+                            return
+
+        # Se não der pra jogar nas laterais, joga no canto oposto
+        opostos = {
+            1: 8, 
+            3: 6, 
+            7: 2, 
+            9: 0
+        }
+        for canto in opostos:
+            if self.tabuleiro[canto] == jogador_adversario and self.tabuleiro[opostos[canto]] == 0:
+                self.fazer_jogada(opostos[canto], self.jogador_atual)
+                return
+
+        # joga aleatoriamente caso nada funcione
+        self.jogada_aleatoria()
 
 
     def mostrar_tabuleiro(self):
@@ -116,32 +155,29 @@ class JogoDaVelha:
 
     def checar_vencedor(self):
         v = self.tabuleiro[1:10]
-
         # linhas
         if (v[0] == v[1] == v[2] != 0) or (v[3] == v[4] == v[5] != 0) or (v[6] == v[7] == v[8] != 0):
             return True
-
         # colunas
         if (v[0] == v[3] == v[6] != 0) or (v[1] == v[4] == v[7] != 0) or (v[2] == v[5] == v[8] != 0):
             return True
-
         # diagonais
         if (v[0] == v[4] == v[8] != 0) or (v[2] == v[4] == v[6] != 0):
             return True
-
         return False
 
-
 # Interface para escolha do tipo de jogo
-print("Escolha o modo de jogo:")
-print("1. Jogador vs Jogador")
-print("2. Jogador vs Aleatório")
-print("3. Jogador vs Campeão")
-print("4. Aleatório vs Aleatório")
-print("5. Aleatório vs Campeão")
-print("6. Campeão vs Campeão")
+modo = 0
+while modo < 1 or modo > 6:
+    print("Escolha o modo de jogo:")
+    print("1. Jogador vs Jogador")
+    print("2. Jogador vs Aleatório")
+    print("3. Jogador vs Campeão")
+    print("4. Aleatório vs Aleatório")
+    print("5. Aleatório vs Campeão")
+    print("6. Campeão vs Campeão")
 
-modo = int(input("Digite o número do modo de jogo desejado: "))
+    modo = int(input("Digite o número do modo de jogo desejado: "))
 num_jogos = 1
 
 if modo in [4, 5, 6]:
